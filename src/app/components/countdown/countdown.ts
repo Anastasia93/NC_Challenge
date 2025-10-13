5a885817554b9710a4c0f657c896f3ba5c0f51d7
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-10-13 14:12:15 +0200

    Feature:
    Adds time counting to Midsummer

--- src/app/components/countdown/countdown.ts
+++ src/app/components/countdown/countdown.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
-  imports: [],
   templateUrl: './countdown.html',
-  styleUrl: './countdown.scss'
+  styleUrls: ['./countdown.scss']
 })
-export class CountdownComponent {
+export class CountdownComponent implements OnInit, OnDestroy {
+  targetDate!: Date;
+  remainingTime: string = '';
+  private intervalId: any;
 
+  ngOnInit() {
+    this.setNextMidsummerEve();
+    this.updateCountdown();
+    this.intervalId = setInterval(() => this.updateCountdown(), 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  private setNextMidsummerEve() {
+    const now = new Date();
+    const year = now.getFullYear();
+
+    let midsummer = new Date(year, 5, 20, 18, 0, 0);
+
+    if (now > midsummer) {
+      midsummer = new Date(year + 1, 5, 20, 18, 0, 0);
+    }
+
+    this.targetDate = midsummer;
+  }
+
+  private updateCountdown() {
+    const now = new Date().getTime();
+    const distance = this.targetDate.getTime() - now;
+
+    if (distance <= 0) {
+      this.remainingTime = "Midsummer Eve is here!";
+      clearInterval(this.intervalId);
+      return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    this.remainingTime = `${days} days, ${hours} h, ${minutes}m, ${seconds}s`;
+  }
 }
