e1466e38e1707958cb75e2271feb6760fca4cd23
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-10-13 13:39:41 +0200

    Feature:
    Adds basic component structure.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { CountdownComponent } from './components/countdown/countdown';
+import { EventFormComponent } from './components/event-form/event-form';
+import { QuoteComponent } from './components/quote/quote';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [CountdownComponent, EventFormComponent, QuoteComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
