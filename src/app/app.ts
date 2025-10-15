7646154ab49abe1309686872195e8df0af3d9080
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-10-15 14:36:13 +0200

    Feature:
    Adds Local Storage saving feature.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-
 import { CountdownComponent } from './components/countdown/countdown';
 import { EventFormComponent } from './components/event-form/event-form';
 import { QuoteComponent } from './components/quote/quote';
@@ -17,19 +16,42 @@ const DEFAULT_EVENT = {
   standalone: true,
   imports: [IfPortraitDirective, CountdownComponent, EventFormComponent, QuoteComponent],
   templateUrl: './app.html',
-  styleUrls: ['./app.scss']
+  styleUrls: ['./app.scss'],
 })
 export class App {
   protected readonly title = signal('nc-challenge');
   orientation: Orientation = Orientation.Landscape;
-  eventTitle = DEFAULT_EVENT.TITLE;
-  eventDate = DEFAULT_EVENT.DATE;
+
+  private _eventTitle: string = DEFAULT_EVENT.TITLE;
+  private _eventDate: string = DEFAULT_EVENT.DATE;
 
   constructor(private orientationService: OrientationService) {}
 
   ngOnInit(): void {
+    const savedTitle = localStorage.getItem('eventTitle');
+    const savedDate = localStorage.getItem('eventDate');
+
+    if (savedTitle) this._eventTitle = savedTitle;
+    if (savedDate) this._eventDate = savedDate;
+
     this.orientationService.orientation$.subscribe(
       (o: Orientation) => (this.orientation = o)
     );
+  }
+
+  get eventTitle(): string {
+    return this._eventTitle;
+  }
+  set eventTitle(value: string) {
+    this._eventTitle = value;
+    localStorage.setItem('eventTitle', value);
+  }
+
+  get eventDate(): string {
+    return this._eventDate;
+  }
+  set eventDate(value: string) {
+    this._eventDate = value;
+    localStorage.setItem('eventDate', value);
   }
 }
